edf87f2d03d40d98380898fee7c0c4cb4cb39180
Amarchand <amarchand@abtach.org>
2023-01-13 06:10:35 +0500

    updated

--- resources/js/components/router/index.js
+++ resources/js/components/router/index.js
@@ -19,7 +19,7 @@ const routes = [
     {
         path: '/admin/login',
         name: 'AdminLogin',
-        component: AdminLogin,
+        component: AdminDashboard,
     },
     {
         path: '/admin/dashboard',
--- resources/views/backend.blade.php
+++ resources/views/backend.blade.php
@@ -1,41 +1,80 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <!-- Basic -->
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <!-- Mobile Metas -->
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <!-- Site Metas -->
-        <meta name="keywords" content="" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <link rel="shortcut icon" href="images/favicon.png" type="">
-        <title>Famms - Admin</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!-- Meta, title, CSS, favicons, etc. -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="images/favicon.ico" type="image/ico" />
 
-        <!-- bootstrap core css -->
-        <link rel="stylesheet" type="text/css" href="web/css/bootstrap.css" />
-        <!-- font awesome style -->
-        <link href="web/css/font-awesome.min.css" rel="stylesheet" />
-        <!-- Custom styles for this template -->
-        <link href="web/css/style.css" rel="stylesheet" />
-        <!-- responsive style -->
-        <link href="web/css/responsive.css" rel="stylesheet" />
+        <title>Gentelella Alela! | </title>
+
+        <!-- Bootstrap -->
+        <link href="/admin/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Font Awesome -->
+        <link href="/admin/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        <!-- NProgress -->
+        <link href="/admin/vendors/nprogress/nprogress.css" rel="stylesheet">
+        <!-- iCheck -->
+        <link href="/admin/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+
+        <!-- bootstrap-progressbar -->
+        <link href="/admin/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+        <!-- JQVMap -->
+        <link href="/admin/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+        <!-- bootstrap-daterangepicker -->
+        <link href="/admin/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+
+        <!-- Custom Theme Style -->
+        <link href="/admin/build/css/custom.min.css" rel="stylesheet">
 
         @vite('resources/css/app.css')
-    </head>
+      </head>
     <body>
         <div id="app"></div>
 
         @vite('resources/js/app.js')
 
-        <!-- jQery -->
-        <script src="web/js/jquery-3.4.1.min.js"></script>
-        <!-- popper js -->
-        <script src="web/js/popper.min.js"></script>
-        <!-- bootstrap js -->
-        <script src="web/js/bootstrap.js"></script>
-        <!-- custom js -->
-        <script src="web/js/custom.js"></script>
+        <!-- jQuery -->
+        <script src="/admin/vendors/jquery/dist/jquery.min.js"></script>
+        <!-- Bootstrap -->
+        <script src="/admin/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- FastClick -->
+        <script src="/admin/vendors/fastclick/lib/fastclick.js"></script>
+        <!-- NProgress -->
+        <script src="/admin/vendors/nprogress/nprogress.js"></script>
+        <!-- Chart.js -->
+        <script src="/admin/vendors/Chart.js/dist/Chart.min.js"></script>
+        <!-- gauge.js -->
+        <script src="/admin/vendors/gauge.js/dist/gauge.min.js"></script>
+        <!-- bootstrap-progressbar -->
+        <script src="/admin/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+        <!-- iCheck -->
+        <script src="/admin/vendors/iCheck/icheck.min.js"></script>
+        <!-- Skycons -->
+        <script src="/admin/vendors/skycons/skycons.js"></script>
+        <!-- Flot -->
+        <script src="/admin/vendors/Flot/jquery.flot.js"></script>
+        <script src="/admin/vendors/Flot/jquery.flot.pie.js"></script>
+        <script src="/admin/vendors/Flot/jquery.flot.time.js"></script>
+        <script src="/admin/vendors/Flot/jquery.flot.stack.js"></script>
+        <script src="/admin/vendors/Flot/jquery.flot.resize.js"></script>
+        <!-- Flot plugins -->
+        <script src="/admin/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+        <script src="/admin/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+        <script src="/admin/vendors/flot.curvedlines/curvedLines.js"></script>
+        <!-- DateJS -->
+        <script src="/admin/vendors/DateJS/build/date.js"></script>
+        <!-- JQVMap -->
+        <script src="/admin/vendors/jqvmap/dist/jquery.vmap.js"></script>
+        <script src="/admin/vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+        <script src="/admin/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+        <!-- bootstrap-daterangepicker -->
+        <script src="/admin/vendors/moment/min/moment.min.js"></script>
+        <script src="/admin/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+        <!-- Custom Theme Scripts -->
+        <script src="/admin/build/js/custom.min.js"></script>
     </body>
 </html>
